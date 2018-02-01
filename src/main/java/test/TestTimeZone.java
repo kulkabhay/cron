e4603d968df57bb61114358c2d34e7a57ef867c2
Abhay Kulkarni <akulkarni@hortonworks.com>
2018-02-01 08:29:31 -0800

    Testing timezone spec

--- src/main/java/test/TestTimeZone.java
+++ src/main/java/test/TestTimeZone.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -36,21 +37,28 @@ public class TestTimeZone {
     static long getStartTimeInMillis(TestTimeZone zone) {
         long ret = zone.startTime.getTime();
 
-        LOG.info("Incoming zone in milliseconds:[" + ret + "]");
+        LOG.info("Incoming startTime:[" + zone.startTime + "]");
+        LOG.info("List of time-zones:[" + Arrays.asList(TimeZone.getAvailableIDs()) +"]");
         if (StringUtils.isNotBlank(zone.timeZone)) {
             TimeZone defaultTZ = TimeZone.getDefault();
             TimeZone zoneTZ = TimeZone.getTimeZone(zone.timeZone);
+            TimeZone UTC = TimeZone.getTimeZone("UTC");
             LOG.info("defaultTZ:[" + defaultTZ +"], zoneTZ:[" + zoneTZ +"]");
+            LOG.info("UTC:[" + UTC +"]");
             if (!defaultTZ.equals(zoneTZ)) {
-                int offset = defaultTZ.getOffset(ret);
+                int offset = zoneTZ.getOffset(ret);
 
-                LOG.info("Offset:[" + offset +"]");
+                LOG.info("Offset of zoneTZ :[" + offset +"]");
+
+                int fromDefault = defaultTZ.getOffset(ret);
+                LOG.info("Offset of defaultTZ :[" + fromDefault +"]");
 
                 ret += offset;
+                ret -= fromDefault;
             }
         }
 
-        LOG.info("Returning zone in milliseconds:[" + ret + "]");
+        LOG.info("Returning modified startTime :[" + (new Date(ret)) + "]");
 
         return ret;
 
